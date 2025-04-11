7d967eb7b8a0a37bd4c01d00c66202f20d1fa94e
Sienna296 <sienna.plissart@etu.u-paris.fr>
2025-04-11 17:49:11 +0200

    Add files via upload

---
+++ Arete.java
@@ -0,0 +1,28 @@
+
+
+public class Arete {
+    private T dep;
+    private T vois;
+    private int val;
+
+    public Arete(T dep, T vois, int val) {
+        this.dep = dep;
+        this.vois = vois;
+        this.val = val;
+    }
+    public T getDep() {
+        return dep;
+    }
+    public T getVois() {
+        return vois;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+
+}
+
+
+
---
+++ T.java
@@ -0,0 +1,28 @@
+import java.util.*;
+package graphe.Graphe.src.sommet;
+
+import java.util.List;
+
+public class T {
+    private int x;
+    private int y;
+    //private List<Arete> voisins;
+
+    public T(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+
+
+
